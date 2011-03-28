c100b43f7df1eed604691be6ba1c12a45d14922e
tm512 <tm512@localhost>
2011-03-27 17:38:05 -0700

    Tweak the install script some more. Set default optimization level back to 2.

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+JOBS=2 # Number of make jobs to use, generally cpu cores x2 if you have hyperthreading.
+
 # Build enet:
 cd enet
-./configure && make || exit 1
+./configure && make -j${jobs} || exit 1
 cd ..
 
 # Configure and compile obsidian:
-./autogen.sh $@ && make || exit 1
+./autogen.sh $@ && make -j${jobs} || exit 1
