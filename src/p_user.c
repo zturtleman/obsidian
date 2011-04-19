4ae807c9dd2e46c89c7ee28b7b5b4240f74a6c06
tm512 <tm512@localhost>
2011-04-18 19:28:50 -0700

    Fix segfault with respawning in a solo-net game

--- src/p_user.c
+++ src/p_user.c
@@ -229,7 +229,8 @@ void P_DeathThink (player_t* player)
     if (player->cmd.buttons & BT_USE)
 	{
 		player->playerstate = PST_REBORN;
-		CL_SendReborn();
+		if(client)
+			CL_SendReborn();
 	}
 }	
 
