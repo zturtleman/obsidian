3e7a43c91b5cc998950bb06f672dab5c135a48ed
tm512 <tm512@localhost>
2011-04-21 14:54:25 -0700

    Remove more extraneous files. Remove version "0.0b", will implement a much better system later.

--- config.h
+++ config.h
@@ -68,7 +68,7 @@
 #define PACKAGE_NAME "obsidian"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "obsidian 0.0b"
+#define PACKAGE_STRING "obsidian"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "obsidian"
@@ -77,10 +77,10 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.0b"
+#define PACKAGE_VERSION ""
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.0b"
+#define VERSION ""
