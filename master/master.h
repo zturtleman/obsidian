82b34a40c5430bf3edf1155add62049fdc050317
tm512 <tm512@localhost>
2011-07-24 12:10:11 -0700

    branch: masterserver Add master server and launcher response protocol

---
+++ master/master.h
@@ -0,0 +1,15 @@
+//
+// Obsidian Master Server
+// [c] 2011 tm512, All Rights Reserved
+//
+// This file is licensed under the WTFPL,
+// The Do What the Fuck You Want To Public License,
+// under the condition that you leave me alone about it.
+
+#ifndef __MASTER_H__
+#define __MASTER_H__
+
+#define SERVER_CHALLENGE 0x45
+#define LAUNCHER_CHALLENGE 0x46
+
+#endif
